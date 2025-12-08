c641054b07055a8d8d4e4c4e827a210f12bc6a8e
drstudioyaya <drstudio.yaya@gmail.com>
2025-12-08 14:54:52 -0800

    Update section1.js

--- questions/premium/test1/section1.js
+++ questions/premium/test1/section1.js
@@ -1,1 +1,40 @@
+window.IELTS_PREMIUM = window.IELTS_PREMIUM || {};
+
+window.IELTS_PREMIUM["test1_section1"] = {
+    audio: "/audio/premium/Listening_Test_1_SECTION_1.mp3",
+
+    questions: [
+        { number: 1, question: "Child’s Name:", answer: "Ethan Park" },
+        { number: 2, question: "Age:", answer: "9" },
+        { number: 3, question: "Programme Start Date:", answer: "July 14" },
+        { number: 4, question: "Basic Fee:", answer: "480" },
+        { number: 5, question: "Meal Plan Fee:", answer: "16" },
+        { number: 6, question: "Allergy Information:", answer: "peanut" },
+
+        {
+            number: 7,
+            question: "Which programme is suitable for Ethan?",
+            options: ["Adventure Camp", "Explorer Camp", "Science Camp"],
+            answer: "Explorer Camp",
+        },
+        {
+            number: 8,
+            question: "How long does the programme last?",
+            options: ["One week", "Two weeks", "Three weeks"],
+            answer: "Two weeks",
+        },
+        {
+            number: 9,
+            question: "How will the woman pay?",
+            options: ["By credit card", "By bank transfer", "In cash"],
+            answer: "By bank transfer",
+        },
+        {
+            number: 10,
+            question: "What will the woman receive after completing the online form?",
+            options: ["A confirmation letter", "A payment receipt", "An email containing bank details"],
+            answer: "An email containing bank details",
+        },
+    ]
+};
 
